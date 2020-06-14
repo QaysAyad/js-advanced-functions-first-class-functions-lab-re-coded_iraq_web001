10d92089e71b1d9c07fd4d37ccf257406c77291d
Qays Ayad <work.worker55@gmail.com>
2020-06-14 13:33:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const  returnFirstTwoDrivers=(drivers)=>{
+  return [drivers[0],drivers[1]];
+}
+
+const  returnLastTwoDrivers=(drivers)=>{
+  return [drivers[drivers.length-2],drivers[drivers.length-1]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(int){
+  return function(fare){
+    return int*fare;
+  }
+}
